fafa8ed2987ae52674198ca043d7f8602c6e4f2b
liana13555 <barishovaliana@gmail.com>
2023-01-18 14:45:05 +0100

    Created selector & updated btn's property

--- mediumclone-angular/src/app/auth/components/register/register.component.ts
+++ mediumclone-angular/src/app/auth/components/register/register.component.ts
@@ -1,7 +1,10 @@
 import {Component, OnInit} from '@angular/core'
 import {FormGroup, FormBuilder, Validators} from '@angular/forms'
-import {Store} from '@ngrx/store'
+import {select, Store} from '@ngrx/store'
+import {Observable} from 'rxjs'
+import {AppStateInterface} from 'src/app/shared/types/appState.interface'
 import {registerAction} from '../../store/actions/register.action'
+import {isSubmittingSelector} from '../../store/selectors'
 
 @Component({
   selector: 'mc-register',
@@ -10,11 +13,16 @@ import {registerAction} from '../../store/actions/register.action'
 })
 export class RegisterComponent implements OnInit {
   form: FormGroup
+  isSubmitting$: Observable<boolean>
 
-  constructor(private fb: FormBuilder, private store: Store) {}
+  constructor(
+    private fb: FormBuilder,
+    private store: Store<AppStateInterface>
+  ) {}
 
   ngOnInit(): void {
     this.initializeForm()
+    this.initializeValues()
   }
 
   initializeForm(): void {
@@ -23,6 +31,11 @@ export class RegisterComponent implements OnInit {
       email: '',
       password: '',
     })
+  }
+
+  initializeValues(): void {
+    this.isSubmitting$ = this.store.pipe(select(isSubmittingSelector))
+    // console.log('isSubmitting$', this.isSubmitting$)
   }
 
   onSubmit(): void {
