1b4d1f1713370a469f27067b19444b692e2940b3
liana13555 <barishovaliana@gmail.com>
2023-01-26 15:09:22 +0100

    Created service & store for module AddToFavorites

--- mediumclone-angular/src/app/shared/modules/addToFavorites/addToFavorites.module.ts
+++ mediumclone-angular/src/app/shared/modules/addToFavorites/addToFavorites.module.ts
@@ -1,10 +1,14 @@
 import {NgModule} from '@angular/core'
 import {CommonModule} from '@angular/common'
 import {AddToFavoritesComponent} from './components/addToFavorites.component'
+import {AddToFavoritesService} from './services/addToFavorites.service'
+import {EffectsModule} from '@ngrx/effects'
+import {AddToFavoritesEffect} from './store/effects/addToFavorites.effect'
 
 @NgModule({
   declarations: [AddToFavoritesComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, EffectsModule.forFeature([AddToFavoritesEffect])],
   exports: [AddToFavoritesComponent],
+  providers: [AddToFavoritesService],
 })
 export class AddToFavoritesModule {}
---
+++ mediumclone-angular/src/app/shared/modules/addToFavorites/store/effects/addToFavorites.effect.ts
@@ -0,0 +1,39 @@
+import {Injectable} from '@angular/core'
+import {Actions, createEffect, ofType} from '@ngrx/effects'
+import {switchMap, map, catchError} from 'rxjs/operators'
+import {of} from 'rxjs'
+import {AddToFavoritesService} from '../../services/addToFavorites.service'
+import {
+  addToFavoritesAction,
+  addToFavoritesFailureAction,
+  addToFavoritesSuccessAction,
+} from '../actions/addToFavorites.action'
+import {ArticleInterface} from 'src/app/shared/types/article.interface'
+
+@Injectable()
+export class AddToFavoritesEffect {
+  addToFavorites$ = createEffect(() =>
+    this.actions$.pipe(
+      ofType(addToFavoritesAction),
+      switchMap(({isFavorited, slug}) => {
+        const article$ = isFavorited
+          ? this.addToFavoritesService.removeFromFavorites(slug)
+          : this.addToFavoritesService.addToFavorites(slug)
+
+        return article$.pipe(
+          map((article: ArticleInterface) => {
+            return addToFavoritesSuccessAction({article})
+          }),
+          catchError(() => {
+            return of(addToFavoritesFailureAction())
+          })
+        )
+      })
+    )
+  )
+
+  constructor(
+    private actions$: Actions,
+    private addToFavoritesService: AddToFavoritesService
+  ) {}
+}
